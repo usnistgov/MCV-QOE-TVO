90bcbba09089c28bb645523323752b2a76ba9445
Eyeson <ene@NIST.GOV>
2021-05-19 11:07:49 -0600

    Added fsf.m and test_fsf.m

---
+++ fsf.py
@@ -0,0 +1,124 @@
+import math
+import mcvqoe
+import numpy as np
+from scipy import signal
+import warnings
+
+def fsf(tx_data, rx_data, fs=48E3):
+    """
+    Purpose
+    -------
+    Frequency slope fit distortion detector using Uniform Bands.
+    
+    Parameters
+    ----------
+    tx_data : numpy array
+        Transmit audio data.
+    rx_data : numpy array
+        Received audio data.
+    fs : double, optional
+        Sample rate. The default is 48E3.
+  
+    Returns
+    -------
+    score : double
+        FSF score.
+
+    """
+    
+    _, delay_samples = mcvqoe.ITS_delay_est(rx_data, tx_data, mode='f', fsamp=48000)
+    if delay_samples < 0:
+        #raise ValueError('Negative delay detected.')
+        warnings.warn('Negative delay detected. Setting delay to zero.')
+        
+    if len(rx_data) < (len(tx_data) + delay_samples):
+        rx_data = rx_data[delay_samples:]
+    else:
+        rx_data = rx_data[delay_samples:(len(tx_data) + delay_samples)]
+    
+    tx_slope = calc_slope(tx_data)
+    
+    rx_slope = calc_slope(rx_data)
+        
+    score = np.true_divide(rx_slope, tx_slope)
+    return score
+    
+def calc_slope(wav_data):
+    """
+    Purpose
+    -------
+    Calculates frequency slope fit for a 
+    given audio clip.
+    
+    Parameters
+    ----------
+    wav_data : numpy array
+        Audio data.
+    band_x : bool
+        Determines whether or not linear fit x values
+        come from band centers (false) or band numbers
+        (true).
+
+    Returns
+    -------
+    slope : double
+        Slope of wav_data.
+
+    """
+    freq_set = np.array([[200, 450], 
+                [400, 650], 
+                [600, 850], 
+                [800, 1050], 
+                [1000, 1250],
+                [1200, 1450],
+                [1400, 1650], 
+                [1600, 1850],
+                [1800, 2050],
+                [2000, 2250],
+                [2200, 2450],
+                [2400, 2650],
+                [2600, 2850],
+                [2800, 3050], 
+                [3000, 3250]])
+    
+    fft_len = 2 ** 14
+    
+    num_bands = len(freq_set)
+    
+    wav_data = wav_data.T
+    
+    #TODO: Explore length of hamming window - see if length of window should be less than length of input wav data 
+    win = signal.get_window('hamming', len(wav_data))
+    #win_1 = signal.windows.hamming(len(wav_data))
+    win = win * wav_data
+    #win_3 = win_1 * wav_data
+   
+    #freq, pxx = signal.periodogram(wav_data, fs=48E3, window=win, nfft=fft_len, detrend=False, scaling='spectrum')
+    #win = win * wav_data
+    freq, pxx = signal.periodogram(win, fs=48E3, nfft=fft_len, scaling='spectrum')
+    #freq, pxx = signal.periodogram(wav_data, fs=48E3, nfft=fft_len, detrend=False, scaling='spectrum')
+     
+    band_vals = np.zeros((num_bands))
+    
+    #TODO: Look into list comprehension 
+    for band in range(num_bands):
+        mask = np.logical_and((freq >= freq_set[band, 0]), (freq <= freq_set[band, 1]))
+        band_vals[band] = 10 * math.log10(np.mean(pxx[mask]))
+            
+    max_idx = np.argmax(band_vals[0:round(num_bands/2)])
+        
+    fit_range = np.arange(max_idx, band_vals.shape[0])
+        
+    x_vals = np.arange(0, num_bands)
+    x_vals = x_vals[fit_range]
+            
+    p = np.polynomial.polynomial.polyfit(x_vals, band_vals[fit_range], 1)
+    
+    slope = p[0]
+    
+    return slope
+    
+
+
+
+
---
+++ test_fsf.py
@@ -0,0 +1,25 @@
+import os
+import mcvqoe
+import unittest
+#import numpy as np
+import scipy.io.wavfile as wav
+from fsf import fsf
+
+class TestEval_FSF(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        #audio_dir = os.path.join('cfs2w.nist.gov', '671', 'Projects', 'MCV', 'Volume-test', 'Volume Impact Project', 'Pytesting', 'eval_FSF Test Cases')
+        cls.audio_dir = os.path.join(os.curdir, 'eval_FSF Test Cases')
+    
+    #def evaluate_csv(python_csv, matlab_csv, diff_csv):
+        
+    def test_eval_fsf(self):
+        _, tx_data = wav.read(os.path.join(self.audio_dir, 'F3_Loud_Norm_DiffNeigh_VaryFill.wav')) 
+        _, rx_data = wav.read(os.path.join(self.audio_dir, 'Rx1_dly_corrected.wav'))
+        tx_data = mcvqoe.audio_float(tx_data)
+        rx_data = mcvqoe.audio_float(rx_data)
+        score = fsf(tx_data, rx_data)
+        self.assertEqual(score, 1.036)
+
+if __name__ == '__main__':
+    unittest.main()
