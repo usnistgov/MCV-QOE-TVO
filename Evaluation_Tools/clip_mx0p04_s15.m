fe567df143200dd7eea56f405a7251183fff3131
Chelsea <cjg2@NIST.GOV>
2021-08-12 09:08:41 -0600

    Added additional scripts to folder

---
+++ Evaluation_Tools/clip_mx0p04_s15.m
@@ -0,0 +1,25 @@
+function y=clip_mx0p04_s15(x,vol)
+%CLIP_MX0P04_S15 example clipping function for distortSim
+%   y=CLIP_MX0P04_S15(x,vol) clips audio vector, x, based on volume vol.
+%   Clipping starts for volumes above -15 dB and the level that audio is
+%   clipped to decreases from there so that the clipping level at 0 dB is
+%   0.04
+%
+%See also: distortSim
+%
+
+    %point with no clipping possible
+    bp=-15;
+    
+    %clip level at zero volume
+    zlvl=0.04;
+    
+    %calculate slope
+    m=(1-zlvl)/(bp);
+    
+    %calculate threshold for 
+    thresh=m*vol+zlvl;
+    
+    %clip waveform
+    y=sign(x).*min(abs(x),thresh);
+end
