743d3f86b379dcad753370cf083733373137cfb4
Chelsea <cjg2@NIST.GOV>
2021-08-12 13:04:35 -0600

    Added a Gsec fix

---
+++ Evaluation_Tools/max_Gsec.m
@@ -0,0 +1,6 @@
+classdef max_Gsec 
+%MAX_Gsec - a class to squash warnings
+%
+%The MAX_GSEC class was originally used in creating some datafiles. It is
+%no longer needed, so this file is in it's place to prevent warnings.
+end
